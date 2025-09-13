473c2e38e40964e5996b5d227eaa6ec50ae2548f
MenlinYan <menlinyan@gmail.com>
2025-09-13 10:20:14 +0800

    YOGA PC自编写代码上传，用于备份对比查找PCN_base拟合困难的问题

---
+++ PCN_base_autoGrad.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from PCN_Visual import PCN_Visual
+
+class PCNBaseAutoGrad(nn.Module):
+    def __init__(self, dims, device="cpu"):
+        super(PCNBaseAutoGrad, self).__init__()
+        self.dims = dims
+        # self.n_layers = len(dims) - 1
+        self.n_layers = len(dims)
+        self.weights = nn.ParameterList([
+            nn.Parameter(torch.randn(d_in, d_out) * 0.1)
+            for d_in, d_out in zip(self.dims[:-1], self.dims[1:])
+        ])
+
+        self.optim_weights = torch.optim.Adam(self.weights, lr=0.01)
+        self.visual_fig = PCN_Visual(depth=self.n_layers, device=device, fixed_xlim=(-5, 5), fixed_ylim=(-5, 5))
+
+
+    def compute_energy(self, x_list, v_list):
+        energy = 0
+        for i in range(self.n_layers - 1):
+            energy += F.mse_loss(v_list[i+1], x_list[i+1], reduction='sum')
+        return energy
+    
+    def forward(self, x_list):
+        v_list = [x_list[0]]
+        for i in range(len(self.weights)):
+            v_list.append(x_list[i] @ self.weights[i])
+            v_list[-1].retain_grad()
+        return v_list
+    
+    def inference(self, x, y=None, n_iter=5, lr=0.1, visualize=False):
+        x_list = [x]
+        # v_list = [x]
+        # training phase or prediction phase
+        for i in range(self.n_layers - 1):
+            # v_list.append(x_list[-1] @ self.weights[i])
+            x_next = x_list[-1] @ self.weights[i]
+            x_list.append(x_next.clone().detach().requires_grad_(True))
+        # fix y
+        if not y is None:
+            x_list[-1].requires_grad = False
+            x_list[-1] = y.detach()
+        # auto grad x
+        begin = 1
+        end = self.n_layers if y is None else self.n_layers - 1
+        for it in range(n_iter):
+            v_list = self.forward(x_list)
+            E = self.compute_energy(x_list, v_list)
+            for x in x_list[begin:end]: 
+                if x.grad is not None: x.grad.zero_()
+            E.backward(retain_graph=True)
+            for i, x in enumerate(x_list[begin:end], start=begin):
+                x.data -= lr * x.grad
+            if it % 10 == 0 or it == n_iter - 1:
+                print(f'Phase-{"training infer" if y is not None else "prediction infer"} Iter {it}, Energy: {E.item()}')
+            if visualize:
+                self.visual_fig.set_x([xi[0].detach().cpu() for xi in x_list])
+                self.visual_fig.set_v([vi[0].detach().cpu() for vi in v_list])
+                self.visual_fig.visualize(phase="training infer" if y is not None else "prediction infer", rounds=it)
+        return x_list, v_list
+
+    def train_step(self, x, y, n_iter=50, lr_x=0.1, lr_w=0.001):
+        x_list, v_list = self.inference(x, y, n_iter, lr=0.5, visualize=True)
+        E = self.compute_energy(x_list, v_list)
+        self.optim_weights.zero_grad()
+        E.backward(retain_graph=True)
+        self.optim_weights.step()
+        return E.item()
+
+    def fit(self, train_loader, batch_size=32, n_epochs=10, n_iter=50, lr_x=0.1, lr_w=0.001):
+        for epoch in range(n_epochs):
+            epoch_loss = 0  
+            for n_batch, (batch_x, batch_y) in enumerate(train_loader):
+                assert batch_x.size(0) == batch_size
+                epoch_loss += self.train_step(batch_x, batch_y, n_iter, lr_x, lr_w)
+            print(f'Epoch {epoch}, Energy: {epoch_loss / len(train_loader)}')
+            
+if __name__ == "__main__":
+    model = PCNBaseAutoGrad([2, 2, 2, 2])
+    x = torch.randn(2,2)*10
+    y = torch.randn(2,2)*10
+    model.fit([(x, x)], batch_size=2, n_epochs=20, n_iter=200)
+    x_list, v_list = model.inference(x, n_iter=20)
+    print(x)
+    print(x_list[-1])
+    print(x)
+    
